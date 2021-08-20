a2e137c01edda28935e7a323dc4c11402878f87f
Hyomin Chang <hyominchang@gmail.com>
2021-08-20 23:20:37 +0900

    Change the packagejson to publish the page

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const number = 1;
+  
+  const styles = {
+      color: 'orange',
+      fontSize: '60px',
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{number > 10 ? number + '은 10보다 크다' : number + '은 10보다 작다'}</p>
+      <p style={styles}>orange</p>
     </div>
   );
 }
